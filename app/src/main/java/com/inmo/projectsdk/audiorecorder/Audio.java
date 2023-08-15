21ca33ad54ca8deaf17ce291dcf02da3dbc0d907
Your Name <you@example.com>
2023-08-15 20:38:33 +0500

    fixes the issue in audio class and add play functionality

--- app/src/main/java/com/inmo/projectsdk/audiorecorder/Audio.java
+++ app/src/main/java/com/inmo/projectsdk/audiorecorder/Audio.java
@@ -1,186 +1,126 @@
 package com.inmo.projectsdk.audiorecorder;
+
+import static android.os.Environment.DIRECTORY_MUSIC;
+
 import android.Manifest;
+import android.content.ContextWrapper;
 import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import androidx.annotation.NonNull;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+
 import com.inmo.projectsdk.R;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Audio extends AppCompatActivity {
 
-    private static final int SAMPLE_RATE_IN_HZ = 44100;
-    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
-    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
-    private static final int RECORD_AUDIO_PERMISSION_CODE = 1;
-    private static final int EXTERNAL_STORAGE_PERMISSION_CODE = 2;
-
-    private Button startButton, stopButton, playButton, pauseButton;
-    private AudioRecord audioRecord;
-    private boolean isRecording = false;
-    private String outputFile;
+    private static final int MICROPHONE_PERMISSION_CODE = 200;
+    final String TAG = "MainAc";
+    //List<String> fileList = new ArrayList<>(),fileList2 = new ArrayList<>();
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+    //RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio_recorder);
 
-        startButton = findViewById(R.id.startButton);
-        stopButton = findViewById(R.id.stopButton);
-        playButton = findViewById(R.id.playButton);
-        pauseButton = findViewById(R.id.pauseButton);
-
-        outputFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/recording.3gp";
-
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startRecording();
-            }
-        });
-
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopRecording();
-            }
-        });
-
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playRecording();
-            }
-        });
-
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pausePlayback();
-            }
-        });
-
-        if (!hasRecordAudioPermission()) {
-            requestRecordAudioPermission();
+        if (isMicrophonePresent()) {
+            getMicrophonePermission();
         }
-
-        if (!hasExternalStoragePermission()) {
-            requestExternalStoragePermission();
-        }
+        Log.d(TAG, "onCreate: " + getRecordingFilePath());
     }
 
-    private boolean hasRecordAudioPermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
-    }
 
-    private boolean hasExternalStoragePermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestRecordAudioPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO_PERMISSION_CODE);
-    }
-
-    private void requestExternalStoragePermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CODE);
-    }
-
-    private void startRecording() {
-        if (!isRecording) {
-            isRecording = true;
-            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ, CHANNEL_CONFIG, AUDIO_FORMAT);
-            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE_IN_HZ, CHANNEL_CONFIG, AUDIO_FORMAT, bufferSize);
-            audioRecord.startRecording();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        FileOutputStream fos = new FileOutputStream(outputFile);
-                        byte[] buffer = new byte[bufferSize];
-                        while (isRecording) {
-                            int read = audioRecord.read(buffer, 0, bufferSize);
-                            if (read > 0) {
-                                fos.write(buffer, 0, read);
-                            }
-                        }
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-    }
-
-    private void stopRecording() {
-        if (isRecording) {
-            isRecording = false;
-            if (audioRecord != null) {
-                audioRecord.stop();
-                audioRecord.release();
-                audioRecord = null;
-            }
-        }
-    }
-    private void playRecording() {
-        MediaPlayer mediaPlayer = new MediaPlayer();
+    public void btnRecordPressed(View view) {
+        Log.d(TAG, "btnRecordPressed: " + getRecordingFilePath());
         try {
-            mediaPlayer.setDataSource(outputFile);
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    mp.release();
-                }
-            });
-            mediaPlayer.prepareAsync();
-        } catch (IOException e) {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(getRecordingFilePath());
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+
+            Toast.makeText(this, "Recording is started", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 
+    public void btnStopPressed(View view) {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
 
-
-
-    private void pausePlayback() {
-        // Pause playback logic...
+        Toast.makeText(this, "Recording is stopped", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void btnPlayPressed(View view) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getRecordingFilePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
 
-        if (requestCode == RECORD_AUDIO_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Record audio permission granted
-            } else {
-                // Record audio permission denied, handle accordingly
-            }
-        } else if (requestCode == EXTERNAL_STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // External storage permission granted
-            } else {
-                // External storage permission denied, handle accordingly
-            }
+            Toast.makeText(this, "Recording is playing", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+    }
+
+    private boolean isMicrophonePresent() {
+        if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void getMicrophonePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, MICROPHONE_PERMISSION_CODE);
+        }
+    }
+
+    private String getRecordingFilePath() {
+
+        List<Integer> ints = new ArrayList<>();
+
+        String child = "testRecordingFile";
+
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File Directory = contextWrapper.getExternalFilesDir(DIRECTORY_MUSIC);
+
+        File file = new File(Directory, child + (getMax(ints) + 1) + ".mp3");
+        Log.d(TAG, "getRecordingFilePath: " + file.getPath() + " " + getMax(ints));
+
+        return file.getPath();
+    }
+
+    public static Integer getMax(List<Integer> list) {
+        if (list == null || list.size() == 0) {
+            return 0;
+        }
+
+        return Collections.max(list);
     }
 }
